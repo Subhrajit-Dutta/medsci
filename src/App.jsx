0748f9bf5b0551447c40c0c9c8d3844b192dba0b
Swapnil Sarkar <swapnilmkab@gmail.com>
2023-04-20 23:09:01 +0530

    Routes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/navigation/navigation.component";
+import Home from "./components/home/home.component";
+import Authentication from "./routes/authentication/authentication.component";
 import "./App.scss";
 
 function App() {
   return (
     <div className="app">
-      <Navigation />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="/auth" element={<Authentication />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Fragment } from "react";
 import "./navigation.styles.scss";
-import { Routes, Route } from 'react-router-dom';
-import Logo from "../../assets/logo.png"
-import Button from '../buttons/nav-button/button.component';
+import Logo from "../../assets/logo.png";
+import Button from "../buttons/nav-button/button.component";
+import { Outlet, Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className='navigation'>
-        <div className='logo-container'>
+    <Fragment>
+      <div className="navigation">
+        <div className="logo-container">
+          <Link to="/">
             <img src={Logo} alt="web-logo" />
+          </Link>
         </div>
-        <div className='nav-wrapper'>
-        <div className='nav-menu'>
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#about">ABOUT</a></li>
-            <li><a href="#contact">CONTACT</a></li>
+        <div className="nav-wrapper">
+          <div className="nav-menu">
+            <li>
+              <a href="#home">HOME</a>
+            </li>
+            <li>
+              <a href="#about">ABOUT</a>
+            </li>
+            <li>
+              <a href="#contact">CONTACT</a>
+            </li>
+          </div>
+            <Button className="button-container" type="submit">
+              <Link to = "/auth">
+              SIGN IN 
+              </Link>
+            </Button>
         </div>
-        <Button className='button-container' type='submit'>SIGN IN</Button>
-        </div>
-    </div>
-  )
-}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
