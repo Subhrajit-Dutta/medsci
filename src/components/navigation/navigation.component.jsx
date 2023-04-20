cafcc9999bf180fdedc1b5871e3a6a0900f3c03d
Swapnil Sarkar <swapnilmkab@gmail.com>
2023-04-20 17:57:15 +0530

    Folder structure updated

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./navigation.styles.scss";
+import { Routes, Route } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div className='navigation'>Navigation</div>
+  )
+}
+
+export default Navigation;
