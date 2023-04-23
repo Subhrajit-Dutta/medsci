f76f5e62af843aa4b4f11d626bcc5e2ac0f471bf
Parrurox <shaikhparwez963@gmail.com>
2023-04-23 12:19:55 +0530

    updated stylings 12:20 pm 23-04-23

--- src/components/profile/profile.component.jsx
+++ src/components/profile/profile.component.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./profile.styles.scss";
 
 const Profile = () => {
-  return <div className="profile"> Profile</div>;
+  return <div className="profile"><h2>Profile</h2>
+  
+  
+  </div>;
 };
 
 export default Profile;
